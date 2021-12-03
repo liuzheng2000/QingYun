666bfc2b3f3ebf8d05419ed03463db03755a29f8
liuzheng2000 <1553385162@qq.com>
2021-12-03 09:47:57 +0800

    2021/12/3

--- src/main/java/STUD/LiuZheng/Test.java
+++ src/main/java/STUD/LiuZheng/Test.java
@@ -3034,3 +3034,55 @@ class TreeNode {
 //        return ans == Integer.MAX_VALUE ?0 :ans;
 //    }
 //}
+
+//class Solution {
+//    public int numIslands(char[][] grid) {
+//        int count = 0;
+//        for (int i = 0; i < grid.length; i++) {
+//            for (int j = 0; j < grid[0].length; j++) {
+//                if (grid[i][j] == '1'){
+//                    inject(grid,i,j);
+//                    count++;
+//                }
+//            }
+//        }
+//        return count;
+//    }
+//
+//    private void inject(char[][] grid, int i, int j) {
+//        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1'){
+//            return;
+//        }
+//        grid[i][j] = '2';
+//        inject(grid,i-1,j);
+//        inject(grid,i+1,j);
+//        inject(grid,i,j-1);
+//        inject(grid,i,j+1);
+//
+//    }
+//}
+
+//class Solution {
+//    public int findCircleNum(int[][] isConnected) {
+//        int provinces = isConnected.length;
+//        boolean[] visited  = new boolean[provinces];
+//        int circles = 0;
+//        Queue<Integer> queue = new LinkedList<>();
+//        for (int i = 0; i < provinces; i++) {
+//            if (!visited[i]){
+//                queue.offer(i);
+//                while (!queue.isEmpty()){
+//                    int j = queue.poll();
+//                    visited[j] = true;
+//                    for (int k = 0; k < provinces; k++) {
+//                        if (isConnected[j][k] == 1 && !visited[k]){
+//                            queue.offer(k);
+//                        }
+//                    }
+//                }
+//                circles++;
+//            }
+//        }
+//        return circles;
+//    }
+//}
